f0b14f9bda1724a0275c0d32d5f4e36ff2eca38b
alperadem15 <alper-gencer@gmx.de>
2025-10-17 23:45:49 +0200

    Initial commit: Spring Boot Projekt für Autovermietung

---
+++ src/main/java/com/autovermietung/web/AutovermietungApplication.java
@@ -0,0 +1,13 @@
+package com.autovermietung.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutovermietungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutovermietungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autovermietung/web/AutovermietungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autovermietung.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutovermietungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
