535439735c149598009de40a5507434278165bfc
alperadem15 <alper-gencer@gmx.de>
2025-10-18 23:42:30 +0200

    Web Controller erstellt

---
+++ src/main/java/com/autovermietung/CarController.java
@@ -0,0 +1,19 @@
+package com.example.autovermietung.controller;
+
+import com.example.autovermietung.model.Car;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class CarController {
+
+    @GetMapping("/cars")
+    public List<Car> getCars() {
+        return List.of(
+                new Car(1L, "VW", "Tiguan", 99.99),
+                new Car(2L, "Mercedes-Benz", "S500", 350.00),
+                new Car(3L, "Tesla", "Model s", 150.00)
+        );
+    }
+}
