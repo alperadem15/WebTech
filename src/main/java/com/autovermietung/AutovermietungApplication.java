99f39961115e61caadc66bf348923a6b82498040
alperadem15 <alper-gencer@gmx.de>
2026-01-11 23:43:49 +0100

    in einen anderen Ordner verschoben

--- src/main/java/com/autovermietung/AutovermietungApplication.java
+++ src/main/java/com/autovermietung/AutovermietungApplication.java
@@ -1,4 +1,4 @@
-package com.autovermietung.web;
+package com.autovermietung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
